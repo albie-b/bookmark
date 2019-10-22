86194b72b04a2a1f38e3ffedb2317a355b2ccdd9
albie-b <55197619+albie-b@users.noreply.github.com>
2019-10-22 14:13:05 +0100

    finished 09

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,8 +1,15 @@
 require 'bookmark'
+require 'pg'
 
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include("http://www.makersacademy.com")
